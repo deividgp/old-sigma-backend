8004e74fee6a12f31d36a665bff79709e3ffc5aa
Deividgp <dayd4@outlook.com>
2022-09-05 17:00:59 +0200

    user routes changed

--- routes/user.js
+++ routes/user.js
@@ -6,12 +6,12 @@ import isAuthenticated from "../utils/isAuthenticated.js";
 const router = Router()
 
 router.post("/newuser", isNotAuthenticated, userControllers.createUser);
-router.post("/:id", isAuthenticated, userControllers.joinServer);
-router.post("/:id", isAuthenticated, userControllers.addFriend);
-router.put("/:id", isAuthenticated, userControllers.updateUser);
-router.put("/:id", isAuthenticated, userControllers.acceptFriend);
-router.delete("/:id", isAuthenticated, userControllers.deleteFriend);
-router.delete("/:id", isAuthenticated, userControllers.deleteUser);
-router.delete("/:id", isAuthenticated, userControllers.leaveServer);
+router.post("/:id/joinserver", isAuthenticated, userControllers.joinServer);
+router.post("/:id/addfriend", isAuthenticated, userControllers.addFriend);
+router.put("/:id/updateuser", isAuthenticated, userControllers.updateUser);
+router.put("/:id/acceptfriend", isAuthenticated, userControllers.acceptFriend);
+router.delete("/:id/deletefriend", isAuthenticated, userControllers.deleteFriend);
+router.delete("/:id/deleteuser", isAuthenticated, userControllers.deleteUser);
+router.delete("/:id/leaveserver", isAuthenticated, userControllers.leaveServer);
 
 export default router;
